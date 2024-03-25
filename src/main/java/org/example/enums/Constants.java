9adb0c07c39067f36cc6fdb1b22937150c404453
NED_OB <ned@workmail.com>
2024-03-25 15:39:27 +0100

    added the file to the main app

--- src/main/java/org/example/enums/Constants.java
+++ src/main/java/org/example/enums/Constants.java
@@ -1,5 +1,6 @@
 package org.example.enums;
 
 public class Constants {
-    public static String FILE_PATH = "C:\\Users\\user\\Desktop\\medic\\lorem-ipsum.txt";
+//    public static String FILE_PATH = "C:\\Users\\user\\Desktop\\medic\\lorem-ipsum.txt";
+    public static String FILE_PATH = "src/main/java/org/example/fileToRead/lorem-ipsum.txt";
 }
